b6ad4de54938620cf4635992b3b63d30d1fd5a99
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-27 23:39:46 +0700

    Tugas 2 ReactJS
    Tugas ini saya kumpulkan kepada Tim Niomic

---
+++ src/Page/Footer.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+   render() {
+      return(
+         <strong>Tugas 2 ReactJS</strong>
+      );
+   }
+}
+
+export default Footer;
